a9ee45bb6463d2e81992000237cfe17dd228f881
Sebastian <sebastian.campbell@sydney.edu.au>
2012-06-12 18:49:49 +1000

    Add subsetting.R

---
+++ Subsetting.R
@@ -0,0 +1,19 @@
+###Some hints:
+
+?seq
+seq(from=1, to=10, by=2)
+#[1] 1 3 5 7 9
+
+#Show the dataframe women that comes with R
+women
+
+#Show the first row of women
+women[1, ]
+
+#Show the first column of women
+women[, 1]
+
+#Show the column of women titled "height"
+women[,"height"]
+
+#Get the 
