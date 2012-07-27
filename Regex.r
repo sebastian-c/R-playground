700d7488a9350e600f033111cf2571c6bd248dcc
Sebastian <sebastian.campbell@sydney.edu.au>
2012-07-27 19:30:05 +1000

    Gave all Regex value=TRUE

--- Regex.r
+++ Regex.r
@@ -7,20 +7,23 @@ d_lets <- paste0(letters, rev(letters))
 d_lets
 #Finding every element with an "a" in it is easy:
 
-which_a_dlets <-grep("a", d_lets)
-d_lets[which_a_dlets]
+grep("a", d_lets, value=TRUE)
 
-#Try finding every element with a "z" or an "o"
+#The `value=TRUE` argument means it outputs the matching values, rather than the
+#indices of these values (try making it FALSE to see what I mean.)
+
+#Try finding every element with a "z" or every element with an "o"
 
 #Let's try finding every one which has either "a" OR "b".
 #To do that, we use square brackets. Square brackets say to find any of the things
 #in the brackets. You can specify as many as you like.
 
-which_ab_dlets <- grep("[ab]", d_lets)
-d_lets[which_ab_dlets]
+grep("[ab]", d_lets, value=TRUE)
 
 #Try finding every element containing a vowel.
 
 #We can also specify a range. To specify all letters from "w" to "z":
 
-which_wz_dlets <- grep("[w-z]", d_lets)
+grep("[w-z]", d_lets, value=TRUE)
+
+#Try finding every element that 
